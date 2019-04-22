7d88c6143a72e3cf15fe375836375f00344969ca
UnluckyJung <ybook2012@gmail.com>
2019-04-22 13:43:42 +0900

    함수 연결과정

--- yaconomy/src/js/app.js
+++ yaconomy/src/js/app.js
@@ -48,13 +48,32 @@ App = {
     var review = $('#review').val();
     var score = $('#score').val();
 
-    console.log(id);
-    console.log(price);
-    console.log(review);
-    console.log(score);
 
-    $('#review').val('');
-    $('#score').val('');
+    web3.eth.getAccounts(function(error,accounts){  //계정을 불러온다.
+      if(error){
+        console.log(error);
+      }
+
+      var account = accounts[0];
+        //위에서 받은것들을 매개변수로 넘김
+      App.contracts.Yamconomy.deployed().then(function(instance){ 
+        //배포가 되었다면 인스턴스를 받아라.
+        var reviewUtf8Encoded = utf8.encode(review);
+        //한글이 쓰이므로 인코딩이 필요함.(utf8.js 라이브러리 추가)
+        return instance.writeReview(id, web3.toHex(reviewUtf8Encoded),score, {from: account, value: price});
+        //매개변수로 data를 넘김
+        //이때 리뷰는 인코딩 한걸 다시 Hex로 변환이 필요함.
+        //0이더를 보냄으로써 기록함.
+
+      }).then(function(){ //잘 작동을 한다면
+        $('#review').val('');//이전에 모달에 입력된 내용들을 지운다.
+        $('#score').val('');
+        $('#writeModal').modal('hide');
+      }).catch(function(err){
+
+        console.log(err.message);
+      });
+    });
 
   },
 
