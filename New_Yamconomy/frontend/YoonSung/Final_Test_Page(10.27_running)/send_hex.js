396c0c8482f83477f05e151034fd08a100565d56
JungYoonSung <ybook2012@gmail.com>
2019-10-27 03:51:12 +0900

    add alert function

--- New_Yamconomy/frontend/YoonSung/Final_Test_Page(10.27_running)/send_hex.js
+++ New_Yamconomy/frontend/YoonSung/Final_Test_Page(10.27_running)/send_hex.js
@@ -1,12 +1,17 @@
 function make_transaction_to_contract(sendnum, h_temp) {
 
+    // if (typeof window.ethereum !== 'undefined'|| (typeof window.web3 !== 'undefined')) {
+    //     alert("plz Install Metamask");  return;
+    // }
+
     const contractInstance = web3.eth.contract(ABI).at("0x63D299614dce54Fe198f09DaDF6EfE04ADA435D7");
     //console.log(contractInstance)
 
     ethereum.enable().then(function (accounts) {
 
         if (accounts.length == 0) {
-            console.log("plz metamask login");
+            alert("plz MetaMask longin");
+            console.log("plz Metamask login");
             return;
         };
         // console.log("get account");
@@ -15,7 +20,13 @@ function make_transaction_to_contract(sendnum, h_temp) {
         console.log(User_account);
 
         contractInstance.Write_Review(sendnum, h_temp, { from: User_account, value: 100 }, function (error, result) { });
+        alert("transaction occured successfully");
+        console.log("transaction occured successfully");
+
+    }).catch(function (error) {
+        alert("plz hashing first");
+        console.error(error)
     });
 
-    console.log("transaction occured successfully");
+
 }
